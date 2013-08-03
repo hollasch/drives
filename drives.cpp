b04b5abab131575334b9e96958120c8d9b034b37
Steve Hollasch <steve@hollasch.net>
2013-08-03 15:00:37 -0700

    Full conversion of tool to Unicode. Converted to 64-bit Visual Studio project.

--- drives.cpp
+++ drives.cpp
@@ -1,10 +1,10 @@
-/*==============================================================================
-
-    drives
-
-    Experimental program to print out volume information about all drives.
-
-==============================================================================*/
+//==================================================================================================
+//
+//  drives
+//
+//  Experimental program to print out volume information about all drives.
+//
+//==================================================================================================
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,138 +15,143 @@
 
 // Program Parameters
 
-const bool fPrintFileSysInfo = false;
+#define fPrintFileSysInfo 0
 
-void  ExpandFileSysFlags (const char *prefix, DWORD flags);
-char *DriveDesc (UINT type);
-
-inline static void print (char *string) { fputs (string, stdout); }
+void     ExpandFileSysFlags (const wchar_t *prefix, DWORD flags);
+wchar_t *DriveDesc (UINT type);
 
 
+//==================================================================================================
+// Simple string print helper function.
+//==================================================================================================
+
+static void print (wchar_t *string)
+{
+    fputws (string, stdout);
+}
+
+
+//==================================================================================================
+// Main Program Entry Point
 //==================================================================================================
 
 int main (int, char* [])
 {
-    DWORD logdrives = GetLogicalDrives();
+    DWORD logicalDrives = GetLogicalDrives();
 
-    char    drive[]  = "A:\\";
-    wchar_t wdrive[] = L"A:";
+    wchar_t drive[]  = L"A:\\";
+    wchar_t volumeLabel[256];
+    wchar_t filesysname [256];
 
     DWORD    netmap_size = 256;
     wchar_t *netmap = new wchar_t [netmap_size];
 
     // Get maximum field lengths.
 
-    int maxLabelLen = 0;
+    size_t maxLabelLen = 0;
 
-    for (drive[0]='A';  drive[0] <= 'Z';  ++ drive[0])
+    for (drive[0]=L'A';  drive[0] <= L'Z';  ++ drive[0])
     {
-        wdrive[0] = L'A' + (drive[0] - 'A');
+        DWORD serialNumber = 0;
+        DWORD maxComponentLength = 0;
+        DWORD fileSysFlags = 0;
 
-        char  volLabel[256];
-        DWORD serialnum = 0;
-        DWORD maxcomponentlen = 0;
-        DWORD filesysflags = 0;
-        char  filesysname [256];
+        auto retval = GetVolumeInformation (
+                          drive, volumeLabel, sizeof(volumeLabel), &serialNumber, &maxComponentLength,
+                          &fileSysFlags, filesysname, sizeof(filesysname));
 
-        bool fVolInfoValid =
-            0 != GetVolumeInformation (
-                     drive, volLabel, sizeof(volLabel), &serialnum,
-                     &maxcomponentlen, &filesysflags,
-                     filesysname, sizeof(filesysname));
-
-        int labelLen = strlen(volLabel);
-
-        if (maxLabelLen < labelLen)
-            maxLabelLen = labelLen;
+        if (retval != 0)
+            maxLabelLen = max (maxLabelLen, wcslen(volumeLabel));
     }
 
     // Print information for each drive.
 
     for (drive[0]='A';  drive[0] <= 'Z';  ++ drive[0])
     {
-        wdrive[0] = L'A' + (drive[0] - 'A');
-
         UINT type = GetDriveType (drive);
 
         if (type == DRIVE_NO_ROOT_DIR)
         {
-            if (logdrives & (1 << (drive[0]-'A')))
+            if (logicalDrives & (1 << (drive[0]-'A')))
             {
-                printf ("%c:   Drive type is %s, but GetLogicalDrives reports "
-                        "no drive there.\n\n", drive[0], DriveDesc(type));
+                wprintf (L"%c:   Drive type is %s, but GetLogicalDrives reports "
+                         L"no drive there.\n\n", drive[0], DriveDesc(type));
             }
             continue;
         }
 
-        char  volLabel[256];
-        DWORD serialnum = 0;
-        DWORD maxcomponentlen = 0;
-        DWORD filesysflags = 0;
-        char  filesysname [256];
+        DWORD serialNumber = 0;
+        DWORD maxComponentLength = 0;
+        DWORD fileSysFlags = 0;
 
-        bool fVolInfoValid =
-            0 != GetVolumeInformation (
-                     drive, volLabel, sizeof(volLabel), &serialnum,
-                     &maxcomponentlen, &filesysflags,
-                     filesysname, sizeof(filesysname));
+        auto retval = GetVolumeInformation (
+                          drive, volumeLabel, sizeof(volumeLabel), &serialNumber, &maxComponentLength,
+                          &fileSysFlags, filesysname, sizeof(filesysname));
+        auto fVolInfoValid = (retval != 0);
 
         // Print drive letter.
 
-        printf ("%c: ", drive[0]);
+        wprintf (L"%c: ", drive[0]);
 
         // Print volume label.
 
-        int labelLen = 0;
-        if (fVolInfoValid && volLabel[0])
+        size_t labelLen = 0;
+        if (fVolInfoValid && volumeLabel[0])
         {
-            printf ("\"%s\"", volLabel);
-            labelLen = strlen(volLabel);
+            wprintf (L"\"%s\"", volumeLabel);
+            labelLen = wcslen(volumeLabel);
         }
         else
         {
-            print  ("  ");
+            print (L"  ");    // Make up for no double quote characters.
         }
 
         // Pad the label to fit the maximum label length.
 
-        if (labelLen < maxLabelLen);
-            printf ("%*s", maxLabelLen - labelLen + 2, "  ");
+        if (labelLen < maxLabelLen)
+            wprintf (L"%*s", maxLabelLen - labelLen, L"");
 
         // Print the drive serial number.
 
         if (!fVolInfoValid)
-            print ("           ");
+            print (L"             ");
         else
-        {
-            printf ("%04x-%04x  ", serialnum >> 16, serialnum & 0xffff);
-        }
+            wprintf (L"  %04x-%04x  ", serialNumber >> 16, serialNumber & 0xffff);
 
         // Print the unique volume name.
 
-        const int max_volname = 50;
-        char volname [max_volname] = "";
+        const int volumeNameMaxLen = 50;
+        wchar_t volumeName [volumeNameMaxLen] = L"";
 
-        if ((0 == GetVolumeNameForVolumeMountPoint (drive, volname, max_volname)) && (volname[0] != 0))
-        {
-            printf ("    Unique Volume Name: %s\n", volname);
-        }
+        if (0 == GetVolumeNameForVolumeMountPoint (drive, volumeName, volumeNameMaxLen))
+            if (volumeName[0] != 0)
+                wprintf (L"    Unique Volume Name: %s\n", volumeName);
 
         // Print the drive description.
 
-        printf ("%s  ", DriveDesc(type));
+        wprintf (L"%s  ", DriveDesc(type));
 
         // Print the file system type.
 
         if (fVolInfoValid && filesysname[0])
-            printf ("[%s]  ", filesysname);
+            wprintf (L"[%s]  ", filesysname);
 
         // Print the net connection, if any.
 
         DWORD netConnRetVal;
 
         do {
-            netConnRetVal = WNetGetConnectionW (wdrive, netmap, &netmap_size);
+            size_t driveNoSlashSize = wcslen(drive) + 1;
+            wchar_t *driveNoSlash = new wchar_t[driveNoSlashSize];
+            wcscpy_s (driveNoSlash, driveNoSlashSize, drive);
+            wchar_t *endPtr = driveNoSlash + wcslen(driveNoSlash) - 1;
+            while ((endPtr >= driveNoSlash) && (*endPtr == '\\'))
+            {
+                *endPtr = 0;
+                --endPtr;
+            }
+
+            netConnRetVal = WNetGetConnectionW (driveNoSlash, netmap, &netmap_size);
 
             if (netConnRetVal == NO_ERROR)
             {   wprintf (L"--> \"%s\"  ", netmap);
@@ -162,78 +167,81 @@ int main (int, char* [])
 
         if (fVolInfoValid)
         {
-            if (fPrintFileSysInfo)
+            #if PrintFileSystemInfo
             {
-                printf ("\n    Max Component Len = %d, FSys Flags = 0x%08x\n",
-                    maxcomponentlen, filesysflags);
-
-                ExpandFileSysFlags ("    ", filesysflags);
+                wprintf (L"\n    Max Component Len = %d, FSys Flags = 0x%08x\n", maxComponentLength, fileSysFlags);
+                ExpandFileSysFlags (L"    ", fileSysFlags);
             }
+            #endif
         }
-        printf ("\n");
+        wprintf (L"\n");
     }
 }
 
 
 
 //==================================================================================================
+// Prints detailed information from the file system flags.
+//==================================================================================================
 
-void ExpandFileSysFlags (const char *prefix, DWORD flags)
+void ExpandFileSysFlags (const wchar_t *prefix, DWORD flags)
 {
-    printf ("%s%s:  Supports named streams\n",
-        prefix, (flags & FILE_NAMED_STREAMS) ? "Yes" : "No ");
+    wprintf (L"%s%s:  Supports named streams\n",
+        prefix, (flags & FILE_NAMED_STREAMS) ? L"Yes" : L"No ");
 
-    printf ("%s%s:  Supports object identifiers\n",
-        prefix, (flags & FILE_SUPPORTS_OBJECT_IDS) ? "Yes" : "No ");
+    wprintf (L"%s%s:  Supports object identifiers\n",
+        prefix, (flags & FILE_SUPPORTS_OBJECT_IDS) ? L"Yes" : L"No ");
 
-    printf ("%s%s:  Supports re-parse points\n",
-        prefix, (flags & FILE_SUPPORTS_REPARSE_POINTS) ? "Yes" : "No ");
+    wprintf (L"%s%s:  Supports re-parse points\n",
+        prefix, (flags & FILE_SUPPORTS_REPARSE_POINTS) ? L"Yes" : L"No ");
 
-    printf ("%s%s:  Supports sparse files\n",
-        prefix, (flags & FILE_SUPPORTS_SPARSE_FILES) ? "Yes" : "No ");
+    wprintf (L"%s%s:  Supports sparse files\n",
+        prefix, (flags & FILE_SUPPORTS_SPARSE_FILES) ? L"Yes" : L"No ");
 
-    printf ("%s%s:  Supports disk quotas\n",
-        prefix, (flags & FILE_VOLUME_QUOTAS) ? "Yes" : "No ");
+    wprintf (L"%s%s:  Supports disk quotas\n",
+        prefix, (flags & FILE_VOLUME_QUOTAS) ? L"Yes" : L"No ");
 
-    printf ("%s%s:  Supports case-sensitive file names\n",
-        prefix, (flags & FS_CASE_SENSITIVE) ? "Yes" : "No ");
+    wprintf (L"%s%s:  Supports case-sensitive file names\n",
+        prefix, (flags & FS_CASE_SENSITIVE) ? L"Yes" : L"No ");
 
-    printf ("%s%s:  Supports file-based compression\n",
-        prefix, (flags & FS_FILE_COMPRESSION) ? "Yes" : "No ");
+    wprintf (L"%s%s:  Supports file-based compression\n",
+        prefix, (flags & FS_FILE_COMPRESSION) ? L"Yes" : L"No ");
 
-    printf ("%s%s:  Supports Encrypted File System (EFS)\n",
-        prefix, (flags & FS_FILE_ENCRYPTION) ? "Yes" : "No ");
+    wprintf (L"%s%s:  Supports Encrypted File System (EFS)\n",
+        prefix, (flags & FS_FILE_ENCRYPTION) ? L"Yes" : L"No ");
 
-    printf ("%s%s:  Supports Unicode file names\n",
-        prefix, (flags & FS_UNICODE_STORED_ON_DISK) ? "Yes" : "No ");
+    wprintf (L"%s%s:  Supports Unicode file names\n",
+        prefix, (flags & FS_UNICODE_STORED_ON_DISK) ? L"Yes" : L"No ");
 
-    printf ("%s%s:  Preserves file name case\n",
-        prefix, (flags & FS_CASE_IS_PRESERVED) ? "Yes" : "No ");
+    wprintf (L"%s%s:  Preserves file name case\n",
+        prefix, (flags & FS_CASE_IS_PRESERVED) ? L"Yes" : L"No ");
 
-    printf ("%s%s:  Preserves and enforces access control lists (ACLs)\n",
-        prefix, (flags & FS_PERSISTENT_ACLS) ? "Yes" : "No ");
+    wprintf (L"%s%s:  Preserves and enforces access control lists (ACLs)\n",
+        prefix, (flags & FS_PERSISTENT_ACLS) ? L"Yes" : L"No ");
 
-    printf ("%s%s:  Volume is compressed\n",
-        prefix, (flags & FS_VOL_IS_COMPRESSED) ? "Yes" : "No ");
+    wprintf (L"%s%s:  Volume is compressed\n",
+        prefix, (flags & FS_VOL_IS_COMPRESSED) ? L"Yes" : L"No ");
 
     return;
 }
 
 
 
-/* ========================================================================= */
+//==================================================================================================
+// Returns the string value for drive type values.
+//==================================================================================================
 
-char *DriveDesc (UINT type)
+wchar_t *DriveDesc (UINT type)
 {
     switch (type)
     {
-        case DRIVE_UNKNOWN:      return "Unknown  ";
-        case DRIVE_NO_ROOT_DIR:  return "No root  ";
-        case DRIVE_REMOVABLE:    return "Removable";
-        case DRIVE_FIXED:        return "Fixed    ";
-        case DRIVE_REMOTE:       return "Remote   ";
-        case DRIVE_CDROM:        return "CD-ROM   ";
-        case DRIVE_RAMDISK:      return "RAM Disk ";
+        case DRIVE_UNKNOWN:      return L"Unknown  ";
+        case DRIVE_NO_ROOT_DIR:  return L"No root  ";
+        case DRIVE_REMOVABLE:    return L"Removable";
+        case DRIVE_FIXED:        return L"Fixed    ";
+        case DRIVE_REMOTE:       return L"Remote   ";
+        case DRIVE_CDROM:        return L"CD-ROM   ";
+        case DRIVE_RAMDISK:      return L"RAM Disk ";
     }
-    return "Unknown type";
+    return L"ERR{Unknown Drive Type}";
 }
