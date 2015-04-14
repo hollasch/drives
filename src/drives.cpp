2b563a444ab47736100e06ed29125eab1b32e0eb
Steve Hollasch <steve@hollasch.net>
2015-04-13 23:22:51 -0700

    First draft of new DriveInfo class.

--- src/drives.cpp
+++ src/drives.cpp
@@ -19,7 +19,8 @@ using namespace std;
 
 // Program Parameters
 
-#define PrintFileSysInfo 0
+#define PrintFileSysInfo  0
+#define PrintNewDriveInfo 0
 
 void     ExpandFileSysFlags (const wchar_t *prefix, DWORD flags);
 wchar_t *DriveDesc (UINT type);
@@ -31,10 +32,99 @@ class DriveInfo
     // This class contains the information for a single drive.
 
   public:
-    DriveInfo() {}
+    DriveInfo (wchar_t driveLetter)
+      : isVolInfoValid(false),
+        netMap(nullptr),
+        serialNumber(0),
+        maxComponentLength(0),
+        fileSysFlags(0),
+        driveType(DRIVE_UNKNOWN)
+    {
+        wcscpy_s (drive, sizeof drive, L"_:\\");
+        drive[0]        = driveLetter;
+
+        wcscpy_s (driveNoSlash, sizeof driveNoSlash, L"_:");
+        driveNoSlash[0] = driveLetter;
+
+        volumeLabel[0] = 0;
+        volumeName[0]  = 0;
+        fileSysName[0] = 0;
+    }
+
+    ~DriveInfo() { }
+
+    void LoadVolumeInformation ()
+    {
+        driveType = GetDriveType (drive);
+
+        isVolInfoValid = (0 != GetVolumeInformation (
+            drive, volumeLabel, sizeof volumeLabel, &serialNumber, &maxComponentLength, &fileSysFlags,
+            fileSysName, sizeof fileSysName));
+
+        if (0 == GetVolumeNameForVolumeMountPoint (drive, volumeName, sizeof volumeName))
+            volumeName[0] = 0;
+
+        // Get the network-mapped connection, if any.
+
+        DWORD netMapBufferSize = MAX_PATH + 1;
+        netMap = new wchar_t[1 + netMapBufferSize];
+        auto retry = false;    // True iff this is a retry.
+
+        do {
+            switch (WNetGetConnectionW (driveNoSlash, netMap, &netMapBufferSize))
+            {
+                case NO_ERROR:
+                    wprintf (L"--> \"%s\"  ", netMap);
+                    break;
+
+                case ERROR_MORE_DATA:
+                    delete[] netMap;
+                    netMap = retry ? nullptr : new wchar_t[netMapBufferSize];
+                    retry = !retry;
+                    break;
+
+                default:
+                case ERROR_BAD_DEVICE:
+                case ERROR_NOT_CONNECTED:
+                case ERROR_CONNECTION_UNAVAIL:
+                case ERROR_NO_NETWORK:
+                case ERROR_EXTENDED_ERROR:
+                case ERROR_NO_NET_OR_BAD_PATH:
+                    delete[] netMap;
+                    netMap = nullptr;
+            }
+
+        } while (retry);
+    }
+
+    void DumpVolumeInformation()
+    {
+        wprintf(L"// Drive \"%s\" / \"%s\":\n"
+                L"    infoValid: %s,\n"
+                L"    label: \"%s\",\n"
+                L"    name: \"%s\",\n"
+                L"    fileSysName: \"%s\",\n"
+                L"    netMap: \"%s\",\n"
+                L"    serialNumber: %016x,\n"
+                L"    maxComponentLength: %d,\n"
+                L"    fileSysFlags: %016x,\n"
+                L"    driveType: %016x\n",
+            drive, driveNoSlash, isVolInfoValid ? L"true" : L"false", volumeLabel, volumeName, fileSysName,
+            netMap ? netMap : L"<null>", serialNumber, maxComponentLength, fileSysFlags, driveType);
+    }
 
   private:
-    
+    wchar_t  drive[1 + sizeof L"X:\\"];        // Drive string with trailing slash (for example, 'X:\').
+    wchar_t  driveNoSlash[1 + sizeof L"X:"];   // Drive string with no trailing slash ('X:').
+    bool     isVolInfoValid;                   // True if we got the drive volume information.
+    wchar_t  volumeLabel[MAX_PATH + 1];        // Drive label
+    wchar_t  volumeName[MAX_PATH + 1];         // Unique volume name
+    wchar_t  fileSysName[MAX_PATH + 1];        // Name of volume file system
+    wchar_t *netMap;                           // If applicable, the network map associated with the drive
+    DWORD    serialNumber;                     // Volume serial number
+    DWORD    maxComponentLength;               // Maximum length for volume path components
+    DWORD    fileSysFlags;                     // Flags for volume file system
+    UINT     driveType;                        // Type of drive volume
 };
 
 
@@ -135,10 +225,9 @@ int main (int, char* [])
 
         // Print the unique volume name.
 
-        const int volumeNameMaxLen = 50;
-        wchar_t volumeName [volumeNameMaxLen] = L"";
+        wchar_t volumeName[MAX_PATH + 1] = L"";
 
-        if (0 == GetVolumeNameForVolumeMountPoint (drive, volumeName, volumeNameMaxLen))
+        if (0 == GetVolumeNameForVolumeMountPoint (drive, volumeName, sizeof volumeName))
             if (volumeName[0] != 0)
                 wprintf (L"    Unique Volume Name: %s\n", volumeName);
 
@@ -154,8 +243,11 @@ int main (int, char* [])
         // Print the net connection, if any.
 
         DWORD netConnRetVal;
+        auto  attempts = 0;
 
         do {
+            ++attempts;
+
             // Create string of the drive with no trailing slash.
             wchar_t driveNoSlash[] = L"*:";
             driveNoSlash[0] = drive[0];
@@ -170,7 +262,9 @@ int main (int, char* [])
                 netmap = new wchar_t[netmapSize];
             }
 
-        } while (netConnRetVal == ERROR_MORE_DATA);
+        } while ((netConnRetVal == ERROR_MORE_DATA) && (attempts < 2));
+
+        wprintf (L"\n");
 
         // Print the volume information.
 
@@ -178,13 +272,20 @@ int main (int, char* [])
         {
             if (isVolInfoValid)
             {
-                wprintf (L"\n    Max Component Len = %d, FSys Flags = 0x%08x\n", maxComponentLength, fileSysFlags);
+                wprintf (L"    Max Component Len = %d, FSys Flags = 0x%08x\n\n", maxComponentLength, fileSysFlags);
                 ExpandFileSysFlags (L"    ", fileSysFlags);
             }
         }
         #endif
 
-        wprintf (L"\n");
+        #if PrintNewDriveInfo
+        {
+            auto driveInfo = new DriveInfo(drive[0]);
+            driveInfo->LoadVolumeInformation();
+            driveInfo->DumpVolumeInformation();
+            delete driveInfo;
+        }
+        #endif
     }
 }
 
