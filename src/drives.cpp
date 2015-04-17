a9bb13f9d97a04a0d790a45cb83b3c779c3d32f4
Steve Hollasch <steve@hollasch.net>
2015-04-17 12:38:53 -0700

    Convert to wstring where possible.

--- src/drives.cpp
+++ src/drives.cpp
@@ -40,61 +40,67 @@ class DriveInfo
 
   public:
     DriveInfo (wchar_t driveLetter)
-      : isVolInfoValid(false),
-        netMap(nullptr),
+      : drive(L"_:\\"),
+        driveNoSlash(L"_:"),
+        isVolInfoValid(false),
         serialNumber(0),
         maxComponentLength(0),
         fileSysFlags(0),
         driveType(DRIVE_UNKNOWN)
     {
-        wcscpy_s (drive, sizeof drive, L"_:\\");
         drive[0]        = driveLetter;
-
-        wcscpy_s (driveNoSlash, sizeof driveNoSlash, L"_:");
         driveNoSlash[0] = driveLetter;
-
-        volumeLabel[0] = 0;
-        volumeName[0]  = 0;
-        fileSysName[0] = 0;
     }
 
     ~DriveInfo() { }
 
     void LoadVolumeInformation ()
     {
-        driveType = GetDriveType (drive);
+        driveType = GetDriveType (drive.c_str());
+
+        wchar_t labelBuffer   [MAX_PATH + 1];   // Buffer for volume label
+        wchar_t fileSysBuffer [MAX_PATH + 1];   // Buffer for file system name
 
         isVolInfoValid = (0 != GetVolumeInformation (
-            drive, volumeLabel, sizeof volumeLabel, &serialNumber, &maxComponentLength, &fileSysFlags,
-            fileSysName, sizeof fileSysName));
+            drive.c_str(), labelBuffer, sizeof labelBuffer, &serialNumber, &maxComponentLength, &fileSysFlags,
+            fileSysBuffer, sizeof fileSysBuffer));
         
-        if (!isVolInfoValid)
+        if (isVolInfoValid)
         {
-            volumeLabel[0]     = 0;
+            volumeLabel = labelBuffer;
+            fileSysName = fileSysBuffer;
+        }
+        else
+        {
+            volumeLabel.clear();
+            fileSysName.clear();
+
             serialNumber       = 0;
             maxComponentLength = 0;
             fileSysFlags       = 0;
-            fileSysName[0]     = 0;
         }
-
-        if (0 == GetVolumeNameForVolumeMountPoint (drive, volumeName, sizeof volumeName))
-            volumeName[0] = 0;
+        
+        wchar_t nameBuffer [MAX_PATH + 1];
+        if (GetVolumeNameForVolumeMountPoint (drive.c_str(), nameBuffer, sizeof nameBuffer))
+            volumeName = nameBuffer;
+        else
+            volumeName.clear();
 
         // Get the network-mapped connection, if any.
 
         DWORD netMapBufferSize = MAX_PATH + 1;
-        netMap = new wchar_t[1 + netMapBufferSize];
+        auto netMapBuffer = new wchar_t[1 + netMapBufferSize];
         auto retry = false;    // True iff this is a retry.
 
         do {
-            switch (WNetGetConnectionW (driveNoSlash, netMap, &netMapBufferSize))
+            switch (WNetGetConnectionW (driveNoSlash.c_str(), netMapBuffer, &netMapBufferSize))
             {
                 case NO_ERROR:
                     break;
 
                 case ERROR_MORE_DATA:
-                    delete[] netMap;
-                    netMap = retry ? nullptr : new wchar_t[netMapBufferSize];
+                    delete[] netMapBuffer;
+                    netMapBuffer = retry ? nullptr : new wchar_t[netMapBufferSize];
                     retry = !retry;
                     break;
 
@@ -105,30 +111,36 @@ class DriveInfo
                 case ERROR_NO_NETWORK:
                 case ERROR_EXTENDED_ERROR:
                 case ERROR_NO_NET_OR_BAD_PATH:
-                    delete[] netMap;
-                    netMap = nullptr;
+                    netMapBuffer[0] = 0;
             }
 
         } while (retry);
+
+        if (netMapBuffer && netMapBuffer[0])
+            netMap = netMapBuffer;
+        else
+            netMap.clear();
+
+        delete[] netMapBuffer;
     }
 
     void GetMaxFieldLengths (size_t &maxLenVolumeLabel)
     {
-        maxLenVolumeLabel = max (maxLenVolumeLabel, wcslen(volumeLabel));
+        maxLenVolumeLabel = max (maxLenVolumeLabel, volumeLabel.length());
     }
 
     void PrintVolumeInformation (size_t maxLenVolumeLabel)
     {
-        wcout << drive[0] << L": ";
+        wcout << driveNoSlash << L' ';
 
         // Print the volume label.
 
         maxLenVolumeLabel += 2;     // Add room for quotes to volume label.
-        auto lenVolumeLabel = (volumeLabel[0] == 0) ? 0 : 2 + wcslen(volumeLabel);
+        auto lenVolumeLabel = (volumeLabel[0] == 0) ? 0 : 2 + volumeLabel.length();
 
         wstring formattedVolumeLabel;
 
-        if (volumeLabel[0])
+        if (volumeLabel.length())
             formattedVolumeLabel.append(L"\"").append(volumeLabel).append(L"\"");
         else
         {
@@ -161,7 +173,7 @@ class DriveInfo
             wcout << L" -       ";
 
         // Mapping, if any.
-        if (netMap)
+        if (netMap.length())
             wcout << L"--> " << netMap;
 
         wcout << endl;
@@ -196,17 +208,17 @@ class DriveInfo
 
   private:
 
-    wchar_t  drive[1 + sizeof L"X:\\"];        // Drive string with trailing slash (for example, 'X:\').
-    wchar_t  driveNoSlash[1 + sizeof L"X:"];   // Drive string with no trailing slash ('X:').
-    bool     isVolInfoValid;                   // True if we got the drive volume information.
-    wchar_t  volumeLabel[MAX_PATH + 1];        // Drive label
-    wchar_t  volumeName[MAX_PATH + 1];         // Unique volume name
-    wchar_t  fileSysName[MAX_PATH + 1];        // Name of volume file system
-    wchar_t *netMap;                           // If applicable, the network map associated with the drive
-    DWORD    serialNumber;                     // Volume serial number
-    DWORD    maxComponentLength;               // Maximum length for volume path components
-    DWORD    fileSysFlags;                     // Flags for volume file system
-    UINT     driveType;                        // Type of drive volume
+    wstring  drive;                 // Drive string with trailing slash (for example, 'X:\').
+    wstring  driveNoSlash;          // Drive string with no trailing slash ('X:').
+    bool     isVolInfoValid;        // True if we got the drive volume information.
+    wstring  volumeLabel;           // Drive label
+    wstring  volumeName;            // Unique volume name
+    wstring  fileSysName;           // Name of volume file system
+    wstring  netMap;                // If applicable, the network map associated with the drive
+    DWORD    serialNumber;          // Volume serial number
+    DWORD    maxComponentLength;    // Maximum length for volume path components
+    DWORD    fileSysFlags;          // Flags for volume file system
+    UINT     driveType;             // Type of drive volume
 
 
     static wchar_t *DriveDesc (UINT type)
